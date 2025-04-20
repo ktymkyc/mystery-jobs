4ac9fb1b06ccec1c8a7011af48d478618db3afdf
ktymkyc <katayama.design@gmail.com>
2025-04-20 16:59:46 +0900

    Update _app.tsx styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,7 @@
-'use client';
-
 import { M_PLUS_Rounded_1c } from 'next/font/google';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import Head from 'next/head'; // ←これ追加！
+import Head from 'next/head';
 
 const mplusRounded = M_PLUS_Rounded_1c({
   weight: ['400', '700', '900'],
@@ -13,13 +11,8 @@ const mplusRounded = M_PLUS_Rounded_1c({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* ここでGAのscriptタグを追加 */}
       <Head>
-        {/* Google Analytics */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-67H5JZG7DK"
-        ></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-67H5JZG7DK"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
