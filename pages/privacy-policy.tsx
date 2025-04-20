109d3052665627306ea8d10ba7a14b8c0db14428
ktymkyc <katayama.design@gmail.com>
2025-04-20 20:11:50 +0900

    Add privacy policy page

---
+++ pages/privacy-policy.tsx
@@ -0,0 +1,9 @@
+// pages/privacy-policy.tsx
+
+'use client';
+
+import PrivacyPolicyPage from '@/components/PrivacyPolicyPage';
+
+export default function PrivacyPolicy() {
+  return <PrivacyPolicyPage />;
+}
